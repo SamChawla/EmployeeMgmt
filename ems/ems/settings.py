922cfc8d78635b1647a1df8cf94abd6c58efc983
Sumit S Chawla <sam.chawla26@gmail.com>
2024-07-24 19:16:36 +0530

    created timesheet app

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Created Apps
+    'project.apps.ProjectConfig',
+    'timesheet.apps.TimesheetConfig',
 ]
 
 MIDDLEWARE = [
